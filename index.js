b22f3210bbffd7985c65ac0d50a816eb5dfc094a
shayaanakbar <shayaanakbar1106@gmail.com>
2019-06-05 13:55:40 -0500

    Hit MVP

--- index.js
+++ index.js
@@ -10,6 +10,34 @@ server.get('/', (req, res) => {
   res.send('testing')
 })
 
+server.post('/api/register', (req, res) => {
+  const user = req.body
+  user.password = bcrypt.hashSync(user.password, 10);
+  console.log(user)
+  Users.addUser(user)
+    .then(resp => res.json(resp))
+    .catch(err => res.json(err))
+});
+
+server.post('/api/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // we compare the password guess against the database hash
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      //
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${user.username}!` });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
 server.get('/api/users', (req,res) => {
   Users.find()
     .then(response => {
